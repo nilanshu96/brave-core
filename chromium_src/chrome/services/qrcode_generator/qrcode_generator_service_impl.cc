4354ae5626655379cc74e114d09a1099081688b1
mkarolin <max@brave.com>
2021-05-28 17:08:53 -0400

    Fixes upstream bug in QRCode generator dino drawing.
    kDinoHeight was used where kDinoWidth should have been used. As a result
    the dino rect ends up being a square and the image is stretched.

---
+++ chromium_src/chrome/services/qrcode_generator/qrcode_generator_service_impl.cc
@@ -0,0 +1,10 @@
+/* Copyright 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#define BRAVE_QRCODE_GENERATOR_SERVICE_IMPL_DRAW_DINO \
+  dino_width_px = pixels_per_dino_tile * dino_image::kDinoWidth;
+
+#include "../../../../../chrome/services/qrcode_generator/qrcode_generator_service_impl.cc"
+#undef BRAVE_QRCODE_GENERATOR_SERVICE_IMPL_DRAW_DINO
